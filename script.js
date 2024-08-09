e88c18c6070dd4eea43d76c8d7c1299455cee568
Devwardhan Kothari <88496527+Devwardhan-Kothari@users.noreply.github.com>
2024-08-09 21:28:26 +0530

    Implement method to get user choice

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ function getComputerChoice()
     return Math.floor(Math.random() * 3) + 1;
 }
 
-
+function getHumanChoice()
+{
+    return prompt("Enter your choice from (Rock, Paper, Scissors)");
+}
 
