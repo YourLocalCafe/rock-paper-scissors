a79f207efcdee4d6eb2e931639004f9f976d3170
Devwardhan Kothari <88496527+Devwardhan-Kothari@users.noreply.github.com>
2024-08-09 21:37:03 +0530

    Fix incorrect return value in getComputerChoice

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function getComputerChoice()
 {
-    return Math.floor(Math.random() * 3) + 1;
+    compChoice = Math.floor(Math.random() * 3) + 1;
+    
+    if(compChoice === 1) return 'rock';
+    else if(compChoice === 2) return 'paper';
+    else return 'scissors';
 }
 
 function getHumanChoice()
